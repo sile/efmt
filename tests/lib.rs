81e3fb00bd8d97928ea49275bfb2faa54795ca51
Takeru Ohta <phjgt308@gmail.com>
2021-11-03 14:29:08 +0900

    Add tests directory

---
+++ tests/lib.rs
@@ -0,0 +1,26 @@
+use efmt::items::forms::Form;
+use efmt::lex::Lexer;
+use efmt::parse::Parser;
+use erl_tokenize::Tokenizer;
+
+#[test]
+fn parse_works() -> anyhow::Result<()> {
+    for entry in std::fs::read_dir("tests/testdata/")? {
+        let entry = entry?;
+        let path = entry.path();
+        if path.extension().map_or(true, |ext| ext != "erl") {
+            continue;
+        }
+
+        let text = std::fs::read_to_string(&path)?;
+        let mut tokenizer = Tokenizer::new(text);
+        tokenizer.set_filepath(path);
+
+        let mut lexer = Lexer::new(tokenizer);
+        let mut parser = Parser::new(&mut lexer);
+        while !parser.is_eof()? {
+            let _: Form = parser.parse()?;
+        }
+    }
+    Ok(())
+}
---
+++ tests/testdata/weird_macro.erl
@@ -0,0 +1,13 @@
+-module(weird_macro).
+
+-define(FOO, /).
+-define(BAR, :format().
+-define(baz(A), A).
+-define(qux, -> [1, 2, 3], [).
+-define(quux, )], [2,).
+-define(a(A, B), A).
+
+-export([?baz(?baz(main)) ?FOO 0]).
+
+hello(A) -> io ?BAR "hello ~p\n", [A]).
+main() ?a(?qux a, b), c], [1, hello(world ?quux 3].
