ca6893c20e0201830a28f60c4fb265317df57476
Takeru Ohta <phjgt308@gmail.com>
2022-06-06 20:53:02 +0900

    Ensure two blank lines are inserted before / after function decls

--- src/items/module.rs
+++ src/items/module.rs
@@ -5,6 +5,8 @@ use crate::parse::{self, Parse, TokenStream};
 use crate::span::{Position, Span};
 use std::num::NonZeroUsize;
 
+const THREE: NonZeroUsize = unsafe { NonZeroUsize::new_unchecked(3) };
+
 /// [Form]*
 #[derive(Debug, Clone, Span)]
 pub struct Module {
@@ -31,8 +33,14 @@ impl Format for Module {
             is_last_spec: false,
             pending_constants: Vec::new(),
         };
+        let mut is_last_fun_decl = false;
 
         for form in &self.forms {
+            if is_last_fun_decl {
+                fmt.add_newlines(THREE);
+                is_last_fun_decl = false;
+            }
+
             if state.pend_if_need(fmt, form) {
                 continue;
             }
@@ -45,6 +53,7 @@ impl Format for Module {
 
             form.format(fmt);
             fmt.add_newline();
+            is_last_fun_decl = form.is_func_decl();
         }
 
         state.flush_pendings(fmt);
@@ -108,14 +117,14 @@ impl<'a> FormatState<'a> {
     }
 
     fn insert_two_empty_newlines_if_need(&mut self, fmt: &mut Formatter, form: &'a Form) {
-        const THREE: NonZeroUsize = unsafe { NonZeroUsize::new_unchecked(3) };
-
         if form.is_func_decl() && !self.is_last_spec {
+            fmt.flush_non_preceding_comments(form);
             fmt.add_newlines(THREE);
         }
 
         self.is_last_spec = form.is_func_spec();
         if form.is_func_spec() {
+            fmt.flush_non_preceding_comments(form);
             fmt.add_newlines(THREE);
         }
     }
