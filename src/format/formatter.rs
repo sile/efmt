7f08837495a3673bb504cd334093e2af57b2f8bd
Takeru Ohta <phjgt308@gmail.com>
2022-07-29 23:31:36 +0900

    Add directive handling

--- src/format/formatter.rs
+++ src/format/formatter.rs
@@ -1,10 +1,16 @@
-use crate::items::tokens::VisibleToken;
+use crate::items::tokens::{CommentToken, VisibleToken};
 
 use crate::parse::TokenStream;
 use crate::span::{Position, Span};
 
 const EOF: Position = Position::new(usize::MAX, usize::MAX, usize::MAX);
 const EOF_MINUS_1: Position = Position::new(usize::MAX - 1, usize::MAX, usize::MAX);
+
+#[derive(Debug)]
+enum Blank {
+    Space(usize),
+    Newline(usize),
+}
 
 #[derive(Debug)]
 pub struct Formatter {
@@ -15,6 +21,8 @@ pub struct Formatter {
     buf: String,
     single_line_mode: bool,
     last_comment_or_macro_position: Option<Position>,
+    skipping: bool,
+    pending_blank: Option<Blank>,
 }
 
 impl Formatter {
@@ -27,6 +35,8 @@ impl Formatter {
             buf: String::new(),
             single_line_mode: false,
             last_comment_or_macro_position: None,
+            skipping: false,
+            pending_blank: None,
         }
     }
 
@@ -84,6 +94,18 @@ impl Formatter {
     pub fn write_span(&mut self, span: &impl Span) {
         let start_position = span.start_position();
         self.write_macros_and_comments(start_position);
+        if span.end_position() <= self.next_position {
+            self.skipping = true;
+            self.pending_blank = None;
+            return;
+        }
+        self.skipping = false;
+
+        match self.pending_blank.take() {
+            None => {}
+            Some(Blank::Space(n)) => self.write_spaces(n),
+            Some(Blank::Newline(n)) => self.write_newlines(n),
+        }
 
         let text = &self.ts.text()[start_position.offset()..span.end_position().offset()];
         self.buf.push_str(text);
@@ -97,10 +119,16 @@ impl Formatter {
             }
         }
 
+        assert!(self.next_position < span.end_position());
         self.next_position = span.end_position();
     }
 
     pub fn write_newlines(&mut self, mut n: usize) {
+        if self.skipping {
+            self.pending_blank = Some(Blank::Newline(n));
+            return;
+        }
+
         for c in self.buf.chars().rev() {
             if c == '\n' {
                 n = n.saturating_sub(1);
@@ -114,6 +142,15 @@ impl Formatter {
     }
 
     pub fn write_newline(&mut self) {
+        if self.skipping {
+            self.pending_blank = Some(Blank::Newline(1));
+            return;
+        }
+
+        if self.buf.is_empty() {
+            return;
+        }
+
         if self.single_line_mode {
             self.write_space();
             return;
@@ -128,11 +165,21 @@ impl Formatter {
     }
 
     pub fn write_space(&mut self) {
+        if self.skipping {
+            self.pending_blank = Some(Blank::Space(1));
+            return;
+        }
+
         self.buf.push(' ');
         self.column += 1;
     }
 
     pub fn write_spaces(&mut self, mut n: usize) {
+        if self.skipping {
+            self.pending_blank = Some(Blank::Space(n));
+            return;
+        }
+
         for c in self.buf.chars().rev() {
             if c == ' ' {
                 n = n.saturating_sub(1);
@@ -173,10 +220,6 @@ impl Formatter {
         self.single_line_mode = mode;
     }
 
-    pub fn skip_formatting(&mut self) {
-        todo!()
-    }
-
     fn write_macros_and_comments(&mut self, next_position: Position) {
         if self.last_comment_or_macro_position == Some(next_position) {
             return;
@@ -192,17 +235,7 @@ impl Formatter {
             if next_comment_start < next_macro_start {
                 let comment = self.ts.comments()[&next_comment_start].clone();
                 self.last_comment_or_macro_position = Some(next_comment_start);
-
-                if comment.is_trailing() {
-                    self.cancel_last_newline();
-                    self.write_spaces(2);
-                }
-                self.write_span(&comment);
-
-                let mode = self.single_line_mode; // TODO
-                self.single_line_mode = false;
-                self.write_newline();
-                self.single_line_mode = mode;
+                self.write_comment(&comment);
             } else {
                 todo!();
                 // let r#macro = self.ts.macros()[&next_macro_start].clone();
@@ -211,6 +244,51 @@ impl Formatter {
                 // self.skip_whitespaces = true;
             }
         }
+    }
+
+    fn write_comment(&mut self, comment: &CommentToken) {
+        let mode = self.single_line_mode;
+        self.single_line_mode = false;
+
+        let mut skip = false;
+        match comment.text(&self.ts.text()).parse() {
+            Err(()) => {}
+            Ok(Directive::FormatOn) => {
+                log::warn!("Found a `@efmt:on` comment at line {} without a preceeding `@efmt:off` (just ignored).",
+                           comment.start_position().line());
+            }
+            Ok(Directive::FormatOff) => {
+                self.next_position = comment.start_position();
+                skip = true;
+            }
+        }
+
+        if skip {
+            self.skip_formatting();
+        } else {
+            if comment.is_trailing() {
+                self.cancel_last_newline();
+                self.write_spaces(2);
+            }
+            self.write_span(comment);
+            self.write_newline();
+        }
+
+        self.single_line_mode = mode;
+    }
+
+    pub fn skip_formatting(&mut self) {
+        let position = self.find_format_on_position(self.next_position);
+        self.write_span(&(self.next_position, position));
+    }
+
+    fn find_format_on_position(&self, current: Position) -> Position {
+        self.ts
+            .comments()
+            .range(current..)
+            .find(|c| matches!(c.text(&self.ts.text()).parse(), Ok(Directive::FormatOn)))
+            .map(|c| c.end_position())
+            .unwrap_or_else(|| Position::new(self.ts.text().len(), usize::MAX, usize::MAX))
     }
 
     fn cancel_last_newline(&mut self) {
@@ -240,328 +318,6 @@ impl Formatter {
             .unwrap_or(EOF)
     }
 }
-
-//     pub fn add_token(&mut self, token: VisibleToken) {
-//         let start_position = token.start_position();
-//         let end_position = token.end_position();
-
-//         self.add_macros_and_comments(start_position);
-
-//         if start_position < self.next_position {
-//             // Macro expanded token.
-//             self.last_skipped_whitespace = None;
-//             return;
-//         }
-//         self.skip_whitespaces = false;
-//         match self.last_skipped_whitespace.take() {
-//             Some(Item::Space) => self.item.add_space(),
-//             Some(Item::Newline { count }) => self.item.add_newlines(count),
-//             None => {}
-//             _ => unreachable!(),
-//         }
-
-//         if let Some(last) = &self.last_token {
-//             if last.needs_space(&token) {
-//                 self.add_space();
-//             }
-//         }
-
-//         self.last_token = Some(token.clone());
-//         self.item.add_token(token);
-
-//         assert!(self.next_position <= end_position);
-//         self.next_position = end_position;
-//     }
-
-//     pub fn add_span(&mut self, span: &impl Span) {
-//         let next_macro_start = self.next_macro_start();
-//         let mut span_start = span.start_position();
-//         let span_end = span.end_position();
-
-//         if next_macro_start != span.start_position() {
-//             self.add_macros_and_comments(span.start_position());
-//             if span_start < self.next_position {
-//                 span_start = self.next_position;
-//             }
-//         }
-
-//         self.item.add_span(&(span_start, span_end));
-//         self.last_token = None;
-//         assert!(self.next_position <= span_end);
-//         self.next_position = span_end;
-//     }
-
-//     pub fn add_space(&mut self) {
-//         if self.skip_whitespaces {
-//             self.last_skipped_whitespace = Some(Item::Space);
-//             return;
-//         }
-//         self.item.add_space();
-//     }
-
-//     pub fn add_newline(&mut self) {
-//         self.add_newlines(NonZeroUsize::new(1).expect("unreachable"));
-//     }
-
-//     pub fn add_newlines(&mut self, count: NonZeroUsize) {
-//         if self.skip_whitespaces {
-//             self.last_skipped_whitespace = Some(Item::Newline { count });
-//             return;
-//         }
-//         self.item.add_newlines(count);
-//     }
-
-//     pub fn add_comment(&mut self, comment: CommentToken) {
-//         if !comment.is_trailing() {
-//             self.add_newline();
-//         }
-//         // Note that two spaces before trailing comments will be added just before writing them
-//         // in `Writer::write_trailing_comment()`.
-
-//         match comment.text(&self.ts.text()).parse() {
-//             Err(()) => {}
-//             Ok(Directive::FormatOn) => {
-//                 log::warn!("Found a `@efmt:on` comment at line {} without a preceeding `@efmt:off` (just ignored).",
-//                            comment.start_position().line());
-//             }
-//             Ok(Directive::FormatOff) => {
-//                 self.next_position = comment.start_position();
-//                 self.skip_formatting();
-//                 return;
-//             }
-//         }
-
-//         self.add_token(VisibleToken::Comment(comment));
-//         self.add_newline();
-//     }
-
-//     fn find_format_on_position(&self, current: Position) -> Position {
-//         self.ts
-//             .comments()
-//             .range(current..)
-//             .find(|c| matches!(c.text(&self.ts.text()).parse(), Ok(Directive::FormatOn)))
-//             .map(|c| c.end_position())
-//             .unwrap_or_else(|| Position::new(self.ts.text().len(), usize::MAX, usize::MAX))
-//     }
-
-//     pub fn format(mut self) -> String {
-//         self.add_macros_and_comments(Position::new(usize::MAX - 1, usize::MAX, usize::MAX));
-//         ItemWriter::new(&self.ts.text()).write_to_string(&self.item)
-//     }
-// }
-
-// #[derive(Debug)]
-// struct ItemWriter<'a> {
-//     writer: Writer,
-//     text: &'a str,
-// }
-
-// impl<'a> ItemWriter<'a> {
-//     fn new(text: &'a str) -> Self {
-//         Self {
-//             writer: Writer::new(),
-//             text,
-//         }
-//     }
-
-//     fn write_to_string(mut self, item: &Item) -> String {
-//         self.write_item(item).expect("bug");
-//         self.writer.finish()
-//     }
-
-//     fn write_item(&mut self, item: &Item) -> Result<()> {
-//         match item {
-//             Item::Region { indent, items, .. } => self.write_region(indent, items)?,
-//             Item::Span {
-//                 start_position,
-//                 end_position,
-//             } => self.write_span(*start_position, *end_position)?,
-//             Item::Token(x) => self.write_token(x)?,
-//             Item::Space => self.writer.write_space()?,
-//             Item::Newline { count } => self.writer.write_newlines(*count)?,
-//         }
-//         Ok(())
-//     }
-
-//     fn write_items(&mut self, items: &[Item]) -> Result<()> {
-//         for item in items {
-//             self.write_item(item)?;
-//         }
-//         Ok(())
-//     }
-
-//     fn write_token(&mut self, token: &VisibleToken) -> Result<()> {
-//         if token.is_trailing_comment() {
-//             self.writer.write_trailing_comment(self.text, token)
-//         } else {
-//             self.writer.write_span(self.text, token, token.is_comment())
-//         }
-//     }
-
-//     fn write_span(&mut self, start_position: Position, end_position: Position) -> Result<()> {
-//         self.writer
-//             .write_span(self.text, &(start_position, end_position), false)
-//     }
-
-//     fn write_region(&mut self, indent: &Indent, items: &[Item]) -> Result<()> {
-//         let indent = match indent {
-//             Indent::Offset(n) => self.writer.current_indent() + n,
-//             Indent::ParentOffset(n) => self.writer.parent_indent() + n,
-//             Indent::CurrentColumnOrOffset(n) => {
-//                 let column = if self.writer.current_column() == 0 {
-//                     self.writer.current_indent()
-//                 } else {
-//                     self.writer.current_column()
-//                 };
-//                 std::cmp::min(column, self.writer.current_indent() + n)
-//             }
-//             Indent::CurrentColumn => {
-//                 if self.writer.current_column() == 0 {
-//                     self.writer.current_indent()
-//                 } else {
-//                     self.writer.current_column()
-//                 }
-//             }
-//             Indent::Absolute(n) => *n,
-//         };
-//         let mut allow_multi_line = true;
-//         let parent_allow_multi_line = self.writer.is_multi_line_allowed();
-
-//         let check_multi_line = items.iter().any(|item| matches!(item, Item::Region { .. }));
-//         if check_multi_line {
-//             allow_multi_line = false;
-//         }
-
-//         let config = RegionConfig {
-//             indent,
-//             allow_multi_line,
-//         };
-//         let result = self.with_subregion(config, |this| this.write_items(items));
-//         if result.is_err() {
-//             if !parent_allow_multi_line {
-//                 return Err(Error::MultiLine);
-//             }
-
-//             let retry = match &result {
-//                 Err(Error::MultiLine) if check_multi_line => true,
-//                 Err(Error::MultiLine) if !allow_multi_line => true,
-//                 _ => false,
-//             };
-//             if retry {
-//                 let config = RegionConfig {
-//                     indent,
-//                     allow_multi_line: true,
-//                 };
-
-//                 return self.with_subregion(config, |this| this.write_items(items));
-//             }
-//         }
-//         result
-//     }
-
-//     fn with_subregion<F>(&mut self, config: RegionConfig, f: F) -> Result<()>
-//     where
-//         F: FnOnce(&mut Self) -> Result<()>,
-//     {
-//         self.writer.start_subregion(config);
-//         let result = f(self);
-//         if result.is_ok() {
-//             self.writer.commit_subregion();
-//         } else {
-//             self.writer.abort_subregion();
-//         }
-//         result
-//     }
-// }
-
-// #[derive(Debug)]
-// enum Item {
-//     Token(VisibleToken),
-//     Span {
-//         start_position: Position,
-//         end_position: Position,
-//     },
-//     Space,
-//     Newline {
-//         count: NonZeroUsize,
-//     },
-//     Region {
-//         indent: Indent,
-//         items: Vec<Item>,
-//     },
-// }
-
-// impl Item {
-//     fn new() -> Self {
-//         Self::Region {
-//             indent: Indent::CurrentColumn,
-//             items: Vec::new(),
-//         }
-//     }
-
-//     fn add_token(&mut self, token: VisibleToken) {
-//         if let Self::Region { items, .. } = self {
-//             if let Some(last @ Self::Region { .. }) = items.last_mut() {
-//                 last.add_token(token);
-//             } else {
-//                 items.push(Self::Token(token));
-//             }
-//         } else {
-//             unreachable!();
-//         }
-//     }
-
-//     fn add_span(&mut self, span: &impl Span) {
-//         if let Self::Region { items, .. } = self {
-//             items.push(Self::Span {
-//                 start_position: span.start_position(),
-//                 end_position: span.end_position(),
-//             });
-//         } else {
-//             unreachable!();
-//         }
-//     }
-
-//     fn add_region(&mut self, region: Item) {
-//         if let Self::Region { items, .. } = self {
-//             items.push(region);
-//         } else {
-//             unreachable!();
-//         }
-//     }
-
-//     fn add_space(&mut self) {
-//         if let Self::Region { items, .. } = self {
-//             if let Some(last @ Self::Region { .. }) = items.last_mut() {
-//                 last.add_space();
-//             } else {
-//                 items.push(Self::Space);
-//             }
-//         } else {
-//             unreachable!();
-//         }
-//     }
-
-//     fn add_newlines(&mut self, count: NonZeroUsize) {
-//         if let Self::Region { items, .. } = self {
-//             items.push(Self::Newline { count });
-//         } else {
-//             unreachable!();
-//         }
-//     }
-
-//     fn is_empty(&self) -> bool {
-//         if let Self::Region { items, .. } = self {
-//             items.iter().all(|item| match item {
-//                 Self::Region { items, .. } => items.is_empty(),
-//                 Self::Space | Self::Newline { .. } => true,
-//                 _ => false,
-//             })
-//         } else {
-//             unreachable!();
-//         }
-//     }
-// }
 
 #[derive(Debug)]
 enum Directive {
@@ -619,7 +375,8 @@ mod tests {
             bar()->bar.
             %% @efmt:on
 
-            -spec baz() -> list().
+            -spec baz() ->
+            list().
             baz()->
                 [1,
                  %% @efmt:off
@@ -627,8 +384,7 @@ mod tests {
                  %% @efmt:on
                  5,6]."},
             indoc::indoc! {"
-            foo() ->
-                foo.
+            foo() -> foo.
 
 
             %% @efmt:off
