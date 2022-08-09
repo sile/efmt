f9012dd71713ae7316f2adbf8efd38cbbbaae14d
Takeru Ohta <phjgt308@gmail.com>
2022-08-09 22:53:10 +0900

    Fix clippy warning

--- src/main.rs
+++ src/main.rs
@@ -144,21 +144,18 @@ impl Opt {
                     _ => {}
                 }
             } else if let Some((k, v)) = item.as_kv_tuple() {
-                match k {
-                    "exclude_file" => {
-                        if let RebarConfigValue::String(v) = v {
-                            match Regex::new(v) {
-                                Ok(regex) => {
-                                    self.exclude_files.push(regex);
-                                }
-                                Err(e) => {
-                                    log::warn!("{v:?} is not a valid regex: {e}");
-                                }
+                if k == "exclude_file" {
+                    if let RebarConfigValue::String(v) = v {
+                        match Regex::new(v) {
+                            Ok(regex) => {
+                                self.exclude_files.push(regex);
                             }
-                            continue;
+                            Err(e) => {
+                                log::warn!("{v:?} is not a valid regex: {e}");
+                            }
                         }
+                        continue;
                     }
-                    _ => {}
                 }
             }
             log::warn!("found an unhandled efmt option in rebar.config: {item:?}");
