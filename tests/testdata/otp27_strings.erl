4220a470e866aa8db2049864eac0da3fae00a3a6
Takeru Ohta <phjgt308@gmail.com>
2024-02-18 20:09:46 +0900

    Add otp27_strings.erl

---
+++ tests/testdata/otp27_strings.erl
@@ -0,0 +1,29 @@
+-module(otp27_strings).
+
+-doc """
+    First line
+    Second line with "\*not emphasized\* Markdown"
+    Third line
+    """.
+
+
+triple_quoted_strings() ->
+    X = <<"""
+        Line 1
+        Line 2
+        """/utf8>>,
+    X = """"
+        ++ foo() ++
+        """",
+    ok.
+
+
+sigil_strings() ->
+    ~"abc\d",
+    ~'abc"d',
+    ~b[abc"d],
+    ~R/^from: /i,
+    ~""""
+     ++ foo() ++
+     """",
+    ok.
