2cb84e4ae56aa370f0a455fd1f4d548d330db831
Takeru Ohta <phjgt308@gmail.com>
2025-11-12 19:16:29 +0900

    chore: update no_mangle attribute syntax for Rust 2024

--- efmt_wasm/src/lib.rs
+++ efmt_wasm/src/lib.rs
@@ -1,6 +1,6 @@
 #![allow(clippy::not_unsafe_ptr_arg_deref)]
 
-#[no_mangle]
+#[unsafe(no_mangle)]
 pub fn check(text: *const u8, text_len: i32) -> *mut Vec<u8> {
     let text = unsafe { std::slice::from_raw_parts(text, text_len as usize) };
     let text = std::str::from_utf8(text).unwrap_or_else(|e| panic!("{e}"));
@@ -12,7 +12,7 @@ pub fn check(text: *const u8, text_len: i32) -> *mut Vec<u8> {
     }
 }
 
-#[no_mangle]
+#[unsafe(no_mangle)]
 pub fn format(text: *const u8, text_len: i32) -> *mut Vec<u8> {
     let text = unsafe { std::slice::from_raw_parts(text, text_len as usize) };
     let text = std::str::from_utf8(text).unwrap_or_else(|e| panic!("{e}"));
@@ -21,22 +21,22 @@ pub fn format(text: *const u8, text_len: i32) -> *mut Vec<u8> {
     Box::into_raw(Box::new(formatted.into_bytes()))
 }
 
-#[no_mangle]
+#[unsafe(no_mangle)]
 pub fn vec_offset(v: *mut Vec<u8>) -> *mut u8 {
     unsafe { &mut *v }.as_mut_ptr()
 }
 
-#[no_mangle]
+#[unsafe(no_mangle)]
 pub fn vec_len(v: *mut Vec<u8>) -> i32 {
     unsafe { &*v }.len() as i32
 }
 
-#[no_mangle]
+#[unsafe(no_mangle)]
 pub fn allocate_vec(len: i32) -> *mut Vec<u8> {
     Box::into_raw(Box::new(vec![0; len as usize]))
 }
 
-#[no_mangle]
+#[unsafe(no_mangle)]
 pub fn free_vec(v: *mut Vec<u8>) {
     let _ = unsafe { Box::from_raw(v) };
 }
