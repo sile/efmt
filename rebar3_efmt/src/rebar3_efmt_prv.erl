5fadbecb1aa8b8c2ffa39273afe68fa4d176b256
Takeru Ohta <phjgt308@gmail.com>
2025-11-22 09:03:23 +0900

    refactor: max_line_lengthをcheck_line_lengthにリネーム

--- rebar3_efmt/src/rebar3_efmt_prv.erl
+++ rebar3_efmt/src/rebar3_efmt_prv.erl
@@ -80,12 +80,12 @@ opts() ->
                              undefined,
                              "Don't raise an error even if the input contains wrong Erlang code."
                              "`efmt` tries to continue formatting the remaining part of the code as much as possible"},
-     {max_line_length, undefined
-                       "check-line-length",
-                       integer,
-                       "Validates that non-comment lines don't exceed the specified length (work with --check). "
-                       "When checking, emits errors if any non-comment line exceeds this limit. "
-                       "This option only validates; it does not reformat code."},
+     {check_line_length, undefined,
+                         "check-line-length",
+                         integer,
+                         "Validates that non-comment lines don't exceed the specified length (work with --check). "
+                         "When checking, emits errors if any non-comment line exceeds this limit. "
+                         "This option only validates; it does not reformat code."},
      {color, undefined, "color", undefined, "Show colored diff. Only applies when `--check` is given."}].
 
 
