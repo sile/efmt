6958050846ae30c618b0d3a47608c518ff7442cb
Takeru Ohta <phjgt308@gmail.com>
2026-02-12 20:16:17 +0900

    feat: support EEP0078 native record syntax

--- efmt_core/src/items/expressions.rs
+++ efmt_core/src/items/expressions.rs
@@ -1,7 +1,7 @@
 //! Erlang expressions.
 use crate::format::Format;
 use crate::items::Expr;
-use crate::items::components::{Either, Element, Parenthesized};
+use crate::items::components::{Element, Parenthesized};
 use crate::items::symbols::OpenBraceSymbol;
 use crate::items::tokens::{
     AtomToken, CharToken, FloatToken, IntegerToken, LexicalToken, SigilStringToken, SymbolToken,
